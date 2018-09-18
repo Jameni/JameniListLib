bff0b0a7eb118c0e74e249101f88d1644c93f4e1
1607613241@qq.com <1607613241@qq.com>
2018-09-18 09:55:30 +0800

    新建项目

---
+++ app/src/main/java/com/jameni/jamenilist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jameni.jamenilist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
